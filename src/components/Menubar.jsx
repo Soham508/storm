83cffea7b2ba4516c4a68324eb4fc286ea7d1e4f
soham508 <sohamghige508@gmail.com>
2025-01-10 16:54:08 +0530

    added router

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/prop-types */
 import { PiFolderOpenLight } from "react-icons/pi";
 import { TfiDownload } from "react-icons/tfi";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 import { FiHelpCircle } from "react-icons/fi";
+//import { useState, useRef } from 'react';
 
-const Menubar = (stageRef) => {
-  console.log(stageRef);
+const Menubar = ({ stageRef, shapes }) => {
+  console.log(stageRef, shapes);
 
   return (
     <div className="flex flex-col p-2 items-start rounded-md bg-[#232329] gap-2 pt-2 pb-2 z-50 absolute top-20 left-4">
@@ -14,7 +16,14 @@ const Menubar = (stageRef) => {
       </button>
       <button className="bg-transparent flex flex-row items-center gap-2 text-slate-100 text-sm w-full hover:bg-zinc-900 focus:outline-0 active:bg-zinc-900/60">
         <TfiDownload size={16} />
-        <span className="items-start">Save on device</span>
+        <span
+          className="items-start"
+          onClick={async () => {
+            console.log(stageRef.current);
+          }}
+        >
+          Save on device
+        </span>
       </button>
       <button className="bg-transparent flex flex-row items-center gap-2 text-slate-100 text-sm w-full hover:bg-zinc-900 focus:outline-0 active:bg-zinc-900/60">
         <IoCloudDownloadOutline size={16} />
